9dd1507e444ea9120af107222c06d56aa5098ccd
Ian Wudarski <email": null>
2016-03-30 14:10:48 -0500

    added question form

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -1,13 +1,18 @@
 get '/questions/new' do
+  if current_user
+  erb :'questions/new'
+  else
+    @message = "please log in"
   erb :'questions/index'
+  end
 end
+
 
 get '/questions/:id' do
   @question = Question.find_by(id: params[:id])
   if @question
     erb :'questions/article'
   else
-    redirect '/'
+    erb :'questions/index'
   end
-
 end
--- app/helpers/session_helpers.rb
+++ app/helpers/session_helpers.rb
@@ -3,5 +3,5 @@ def logged_in?
 end
 
 def current_user
-  @current_user = User.find(session[:user_id])
+  @current_user = User.find_by(id: session[:user_id])
 end
