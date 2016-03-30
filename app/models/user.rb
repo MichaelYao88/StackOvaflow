4ac8dde21b7d3c554cb016da9f2ec4d0b6626ed4
Taylor Daugherty <email": null>
2016-03-30 13:24:55 -0500

    fix nil password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,32 +2,34 @@ require 'bcrypt'
 
 class User < ActiveRecord::Base
   include BCrypt
+  validates :name, presence: true
+  validate :validate_password
+
+  has_many :votes, foreign_key: :voter_id
+  has_many :comments, foreign_key: :commenter_id
+  has_many :questions, foreign_key: :questioner_id
+  has_many :answers, foreign_key: :answerer_id
 
   def password
     @password ||= Password.new(hashed_password)
   end
 
   def password=(new_password)
-    @plain_text= new_password
+    @plain_text = new_password
     @password = Password.create(new_password)
     self.hashed_password = @password
   end
 
   def self.authenticate(name, password)
-    user=User.find_by(name: name)
+    user = User.find_by(name: name)
     return user if user && user.password == password
   end
 
   def validate_password
-    @errors.add(:password, "must be longer than 6 characters") unless @plain_text.length > 6
-    @errors.add(:password, "password can't be blank") if @plain_text.nil?
+    if @plain_text.nil?
+      @errors.add(:password, "password can't be blank")
+    elsif @plain_text.length < 6
+      @errors.add(:password, "must be longer than 6 characters")
+    end
   end
-
-validates :name, presence: true
-validate :validate_password
-
-has_many :votes, foreign_key: :voter_id
-has_many :comments, foreign_key: :commenter_id
-has_many :questions, foreign_key: :questioner_id
-has_many :answers, foreign_key: :answerer_id
 end
