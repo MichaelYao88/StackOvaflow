47b1464f05ff3ff0e74054aa8953b014d5618de9
Taylor Daugherty <email": null>
2016-03-30 14:39:40 -0500

    Add profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,20 @@ get '/users/new' do
 end
 
 post '/users' do
-  user = User.new(name: params[:name], password: params[:password])
+  user = User.new(params[:user])
   if user.save
     session[:user_id] = user.id
-    redirect "/"
+    redirect "/users/show"
   else
     @errors = user.errors.full_messages
     erb :"/users/new"
   end
+end
+
+get '/users/show' do
+  @user= current_user
+  @questions= @user.questions
+  @answers= @user.answers
+  erb :'/users/show'
+
 end
