c5ddb0a4df4988ca5a2644430780412f994deb0f
Ian Wudarski <email": null>
2016-03-30 17:29:56 -0500

    upboats working

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
   $('.upboat').on('click', function(event){
-    event.prevenDefault();
-    $.ajax
-  })
+    event.preventDefault();
+    var url = $(this).attr('href');
+    var $that = $(this)
+
+    $.ajax({
+      type: 'POST',
+      url: url
+
+    }).done(function(data){
+      $that.closest(".parent").find(".upvotes").text(data);
+    });
+  });
 });
