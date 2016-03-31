542e01f3d4ed078e890cf5a8fcc46bfc85ae9679
[01;31m[K"name"[m[K: "Ian Wudarski <[01;31m[K"email"[m[K: null>
2016-03-31 09:47:25 -0500

    seeds and layout change

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,20 +15,6 @@ question2 = whalliam.questions.create!(question_title: "What do whales eat?", qu
 answer2 = question2.answers.create!(answerer: bruce, text: "Fish and ships?"  )
 comment2 = answer2.comments.create!(commenter: whaley_wonka, text: "I don't know what?")
 vote2 = comment2.votes.create!(voter: bruce, value: 1)
-whaley_wonka = User.create!(name: "Whaley Wonka", password: "password");
-whalliam = User.create!(name: "Whalliam", password: "password");
-bruce = User.create!(name: "Bruce Whallis", password: "password");
-
-question1 = whaley_wonka.questions.create!(question_title: "Whale I ever be happy?", question_body: "whale I ever be happy")
-answer1 = question1.answers.create!(answerer: whaley_wonka, text: "spouting makes me happy briefly" )
-comment1 = answer1.comments.create!(commenter: whalliam, text: "I heard if you don't spout in a while...")
-vote1 = comment1.votes.create!(voter: bruce, value: 1)
-
-
-question2 = whalliam.questions.create!(question_title: "What do whales eat?", question_body: "???")
-answer2 = question2.answers.create!(answerer: bruce, text: "Fish and ships?"  )
-comment2 = answer2.comments.create!(commenter: whaley_wonka, text: "I don't know what?")
-vote2 = comment2.votes.create!(voter: bruce, value: 1)
 
 
 #create 10 new users at random
@@ -42,11 +28,35 @@ end
 
 #create 10 new questions
 10.times do
-  Question.create!({
-      question_title: Faker::Hacker.say_something_smart,
-      question_body: Faker::Lorem.sentence
+  user = User.all.sample
+  user.questions.create!({
+      question_title: Faker::Hacker.adjective + " " + Faker::Hipster.word,
+      question_body: Faker::Hipster.sentence
     })
 end
 
+100.times do
+  user = User.all.sample
+  question = Question.all.sample
+  question.answers.create!({
+    answerer: user,
+    text: Faker::Lorem.sentence
+    })
+end
+
+100.times do
+  user = User.all.sample
+  question = Question.all.sample
+  answer = Answer.all.sample
+  question.comments.create!({
+    text: Faker::Hipster.sentence,
+    commenter: user
+    })
+
+  answer.comments.create!({
+    text: Faker::Hipster.sentence,
+    commenter: user
+    })
+end
 
 
