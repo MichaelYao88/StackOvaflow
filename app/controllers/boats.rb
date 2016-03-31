43f0f14fb4e93f1e72fdf33e31bc70e42182e7c8
Ian Wudarski <email": null>
2016-03-31 10:16:51 -0500

    start up/down for commets/answers

--- app/controllers/boats.rb
+++ app/controllers/boats.rb
@@ -5,8 +5,9 @@ post '/upboat/questions/:id' do
   vote.save
   if request.xhr?
     total = question.votes.reduce(0){ |sum, votes| sum + votes.value }
-    (total).to_s
+    total.to_s
   else
+    redirect '/'
   end
 end
 
@@ -17,9 +18,25 @@ post '/downboat/questions/:id' do
   vote.save
   if request.xhr?
     total = question.votes.reduce(0){ |sum, votes| sum + votes.value }
-
-    p (total).to_s
-    # question.votes.count.to_s
+    total.to_s
   else
+    redirect '/'
   end
+end
+
+post '/upboat/answers/:id' do
+  answer = Answers.find(params[:id])
+  vote = answer.votes.find_or_create_by(voter: current_user)
+  vote.value = 1
+  vote.save
+  if request.xhr?
+    total = answer.votes.reduce(0){ |sum, votes | sum + votes.value }
+    total.to_s
+  else
+    redirect '/'
+  end
+end
+
+post '/downboat/answers/:id' do
+
 end
